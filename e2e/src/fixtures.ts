1393ec1038cd0df39d0f3d75e9669120b24eadec
Matt Raible <matt.raible@crowdstrike.com>
2026-05-07 14:13:00 -0600

    Trim unused fixtures from e2e/src/fixtures.ts

--- e2e/src/fixtures.ts
+++ e2e/src/fixtures.ts
@@ -1,37 +1,17 @@
 import { test as baseTest } from '@playwright/test';
 import {
-  FoundryHomePage,
-  AppManagerPage,
-  AppCatalogPage,
   DetectionExtensionPage,
   WorkflowsPage,
   HostManagementPage,
-  config,
 } from '@crowdstrike/foundry-playwright';
 
 type FoundryFixtures = {
-  foundryHomePage: FoundryHomePage;
-  appManagerPage: AppManagerPage;
-  appCatalogPage: AppCatalogPage;
   detectionExtensionPage: DetectionExtensionPage;
   workflowsPage: WorkflowsPage;
   hostManagementPage: HostManagementPage;
-  appName: string;
 };
 
 export const test = baseTest.extend<FoundryFixtures>({
-  foundryHomePage: async ({ page }, use) => {
-    await use(new FoundryHomePage(page));
-  },
-
-  appManagerPage: async ({ page }, use) => {
-    await use(new AppManagerPage(page));
-  },
-
-  appCatalogPage: async ({ page }, use) => {
-    await use(new AppCatalogPage(page));
-  },
-
   detectionExtensionPage: async ({ page }, use) => {
     await use(new DetectionExtensionPage(page));
   },
@@ -42,10 +22,6 @@ export const test = baseTest.extend<FoundryFixtures>({
 
   hostManagementPage: async ({ page }, use) => {
     await use(new HostManagementPage(page));
-  },
-
-  appName: async ({}, use) => {
-    await use(config.appName);
   },
 });
 
